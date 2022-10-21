3e1d3c913f7d5fdc3d9665f383bcf3b8e0b221eb
Junichi Kato <j5ik2o@gmail.com>
2022-10-21 18:36:02 +0900

    wip

--- test/CommandSpec.hs
+++ test/CommandSpec.hs
@@ -10,9 +10,12 @@ spec :: Spec
 spec =
   describe "command" $ do
     it "execute" $ do
-      let mc = MacroCommand [EchoCommand "1", EchoCommand "2", EchoCommand "3"]
-      let echo = EchoCommand "0"
-      forM_ [mc, echo] $ \c -> do
+      let mc = MacroCommand [EchoCommand "0"]
+      let mc2 = append mc $ EchoCommand "1"
+      let mc3 = append mc2 $ EchoCommand "2"
+      let mc4 = append mc3 $ EchoCommand "3"
+      let echo = EchoCommand "4"
+      forM_ [mc4, echo] $ \c -> do
         execute c
       1 `shouldBe` 1
 
