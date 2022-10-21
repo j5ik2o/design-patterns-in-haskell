00d1306de468127b0cafc9a2d0ee852075f1fb0f
Junichi Kato <j5ik2o@gmail.com>
2022-10-21 18:36:50 +0900

    wip

--- test/CommandSpec.hs
+++ test/CommandSpec.hs
@@ -15,8 +15,7 @@ spec =
       let mc3 = append mc2 $ EchoCommand "2"
       let mc4 = append mc3 $ EchoCommand "3"
       let echo = EchoCommand "4"
-      forM_ [mc4, echo] $ \c -> do
-        execute c
+      forM_ [mc4, echo] execute
       1 `shouldBe` 1
 
 
