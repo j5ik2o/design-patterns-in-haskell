adc78701fdece5e2efc5b67ff39ce1e6cfc59bd3
Junichi Kato <j5ik2o@gmail.com>
2022-10-21 18:15:35 +0900

    wip

--- src/Command.hs
+++ src/Command.hs
@@ -7,7 +7,10 @@ execute (EchoCommand message) = putStrLn message
 execute (MacroCommand commands) = mapM_ execute commands
 
 append :: Command -> Command -> Command
-append (MacroCommand commands) command = MacroCommand (commands ++ [command])
+append f@(EchoCommand _) s@(EchoCommand _) = MacroCommand [f, s]
+append f@(EchoCommand _) (MacroCommand commands) = MacroCommand $ f : commands
+append (MacroCommand commands) s@(EchoCommand _) = MacroCommand $ commands ++ [s]
+append (MacroCommand l) (MacroCommand r) = MacroCommand $ l ++ r
 
 undo :: Command -> Command
 undo (MacroCommand commands) = MacroCommand (init commands)
